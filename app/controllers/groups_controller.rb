a1b5305f5bcac81555def23620338046c17c0a79
Alain Thoen <alain.thoen@protime.be>
2015-03-26 16:46:22 +0100

    removed show_bills from group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,10 +30,6 @@ class GroupsController < ApplicationController
     set_group
   end
 
-  def show_bills
-    @bills = set_group.bills
-  end
-
   def destroy
     set_group
     current_user.destroy_group(@group)
