012d2e6c1a39cd1dd22013b64dee4300957571e1
Alain Thoen <alain.thoen@protime.be>
2015-03-26 11:11:50 +0100

    test for bugfix bill create

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe BillsController, :type => :controller do
   login_user
   let(:user) { User.first }
-  let(:valid_attributes) {{amount: 4,user_id: user.id, group_id: 1, description: "Winkel" }}
-  let(:invalid_attributes) {{:user_id => nil,:group_id => nil}}
   let(:group) {FactoryGirl.create(:group, owner_id: user.id)}
+  let(:valid_attributes) {{amount: 4,user_id: user.id, group_id: 1, description: "Winkel" }}
+  let(:invalid_post_attributes) {{:bill => {amount: 'text', description: nil}, groups_select:group.id , user_id:2}}
 
-  let (:post_attributes) {{:bill => {amount: 5, description: "Winkel"}, groups_select:5 , user_id:2}}
+  let (:post_attributes) {{:bill => {amount: 5, description: "Winkel"}, groups_select:group.id , user_id:2}}
 
   describe "GET index" do
     it "assigns all bills as @bills" do
@@ -40,13 +40,24 @@ RSpec.describe BillsController, :type => :controller do
         expect{post :create, post_attributes}.to change(Bill, :count).by(1)
         expect(Bill.last.amount).to eq 5
         expect(Bill.last.user_id).to eq 2
-        expect(Bill.last.group_id).to eq 5
+        expect(Bill.last.group_id).to eq group.id
         expect(Bill.last.description).to eq "Winkel"
       end
 
       it "redirects to the created bill" do
         post :create, post_attributes
         expect(response).to redirect_to(Bill.last)
+      end
+    end
+
+    describe "with invalid params" do
+      it "creates a new Bill" do
+        expect{post :create, invalid_post_attributes}.to change(Bill, :count).by(0)
+      end
+
+      it "redirects to the bill#new" do
+        post :create, invalid_post_attributes
+        expect(response).to redirect_to(new_bill_path(group_id: group.id))
       end
     end
   end
