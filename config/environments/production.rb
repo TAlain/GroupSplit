22772d1b0821f8aacbcfc73211203406d5024dba
Alain Thoen <alain.thoen@protime.be>
2015-04-11 13:43:12 +0200

    pointing to herokuapp for safari dns issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'groupsplit.be' }
-  Rails.application.routes.default_url_options[:host] = 'groupsplit.be'
+  config.action_mailer.default_url_options = { :host => 'groupsplit.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'groupsplit.herokuapp.com'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
